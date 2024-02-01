452737c395ad841ec6096d64cb16e39586631d4c
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-02-01 14:00:56 +0800

    added a single div to container div

---
+++ etch-a-sketch.js
@@ -0,0 +1,5 @@
+const gridContainer = document.querySelector("#gridContainer");
+const singleDiv = document.createElement("div");
+Object.assign(singleDiv.style, { border: "1px solid black", backgroundColor: "yellow", width: "30px", height: "30px" });
+
+gridContainer.appendChild(singleDiv);
