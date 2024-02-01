b58132ce6ba500ea9fa287e481e6aa0b6a89cea9
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-02-01 16:42:26 +0800

    fixed dynamic grid issue: grids not decreasing when range decreases

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,19 +4,18 @@ const gridTitle = document.querySelector("#gridTitle"); //h3
 let gridNumber = document.createElement("span"); // new element to store the value of the range
 const gridRange = document.querySelector("#gridRange"); // references to the input range
 
-// gridNumber.textContent = gridRange.value;
-// gridTitle.appendChild(gridNumber);
-
-// default 8x8 rows
-for (let row = 1; row <= 8; row++) {
-  for (let col = 1; col <= 8; col++) {
-    const column = document.createElement("div");
-    Object.assign(column.style, { border: "1px solid black", backgroundColor: "yellow", gridColumn: `${col} / span 1`, gridRow: `${row} / span 1` });
-    gridContainer.appendChild(column);
-  }
-}
-
+// event listener for appending the dynamic input value to H3 tag
 gridRange.addEventListener("input", () => {
   gridNumber.textContent = gridRange.value;
   gridTitle.appendChild(gridNumber);
+
+  gridContainer.textContent = "";
+  // creating dynamic grids
+  for (let row = 1; row <= gridRange.value; row++) {
+    for (let col = 1; col <= gridRange.value; col++) {
+      const column = document.createElement("div");
+      Object.assign(column.style, { border: "1px solid black", backgroundColor: "yellow", gridColumn: `${col} / span 1`, gridRow: `${row} / span 1` });
+      gridContainer.appendChild(column);
+    }
+  }
 });
