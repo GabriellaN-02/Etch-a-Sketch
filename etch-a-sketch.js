9309768394dc2ac057d867a68469f82976876199
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-02-01 15:26:16 +0800

    created default 16x16 grid board

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,15 @@
 const gridContainer = document.querySelector("#gridContainer");
-const singleDiv = document.createElement("div");
-Object.assign(singleDiv.style, { border: "1px solid black", backgroundColor: "yellow", width: "30px", height: "30px" });
 
-gridContainer.appendChild(singleDiv);
+// Object.assign(column.style, { border: "1px solid black", backgroundColor: "yellow", gridTemplateColumns: "16", gridTemplateRows: "16" });
+// gridContainer.appendChild(column);
+
+gridContainer.style.gridTemplateColumns = 16;
+gridContainer.style.gridTemplateRows = 16;
+
+for (let row = 1; row <= 16; row++) {
+  for (let col = 1; col <= 16; col++) {
+    const column = document.createElement("div");
+    Object.assign(column.style, { border: "1px solid black", backgroundColor: "yellow", gridColumn: `${col} / span 1`, gridRow: `${row} / span 1` });
+    gridContainer.appendChild(column);
+  }
+}
