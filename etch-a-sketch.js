e19b2d6c56e160db598def16006e1bec3f627529
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-02-02 16:21:45 +0800

    added initial functions forr firing repeated mousedown events

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,6 +5,8 @@ let gridNumber = document.createElement("span"); // new element to store the val
 const gridRange = document.querySelector("#gridRange"); // references to the input range
 
 const color = document.querySelector("#color");
+const clickRate = 100;
+let isMouseDown = false;
 
 //default 8 value of grid size in range
 gridNumber.textContent = `${gridRange.value}x${gridRange.value}`;
@@ -18,11 +20,41 @@ for (let row = 1; row <= 8; row++) {
     Object.assign(column.style, { border: "1px solid gray", gridColumn: `${col} / span 1`, gridRow: `${row} / span 1` });
     gridContainer.appendChild(column);
 
-    column.addEventListener("click", () => {
-      column.style.backgroundColor = color.value;
-    });
+    column.addEventListener("mouseenter", handleMouseEnter);
   }
 }
+
+function startClick(column) {
+  column.style.backgroundColor = color.value;
+  column.dataset.isColored = "true";
+  clickTimeout = setTimeout(() => startClick(column), 1000 / clickRate);
+}
+
+function stopClick() {
+  isMouseDown = false;
+  clearTimeout(clickTimeout);
+}
+
+//for continuous coloring
+function handleMouseDown(event) {
+  const column = event.target;
+  isMouseDown = true;
+  if (!column.dataset.isColored) {
+    startClick(column);
+  }
+}
+//checks if the mouse is down and the column is not colored, then it starts the continuous coloring
+function handleMouseEnter(event) {
+  if (isMouseDown) {
+    const column = event.target;
+    if (!column.dataset.isColored) {
+      startClick(column);
+    }
+  }
+}
+
+document.addEventListener("mousedown", handleMouseDown);
+document.addEventListener("mouseup", stopClick);
 
 // Event listener: 1. Dynamic range value 2. Dynamic grid items
 gridRange.addEventListener("input", () => {
@@ -38,9 +70,7 @@ gridRange.addEventListener("input", () => {
       Object.assign(column.style, { border: "1px solid gray", gridColumn: `${col} / span 1`, gridRow: `${row} / span 1` });
       gridContainer.appendChild(column);
 
-      column.addEventListener("click", () => {
-        column.style.backgroundColor = color.value;
-      });
+      column.addEventListener("mouseenter", handleMouseEnter);
     }
   }
 });
