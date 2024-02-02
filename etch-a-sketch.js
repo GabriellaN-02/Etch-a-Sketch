82f029381ba9dc746b712aa7c632b01e4123cc41
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-02-02 13:53:31 +0800

    created default 8x8 grids

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,7 +4,20 @@ const gridTitle = document.querySelector("#gridTitle"); //h3
 let gridNumber = document.createElement("span"); // new element to store the value of the range
 const gridRange = document.querySelector("#gridRange"); // references to the input range
 
-// event listener for appending the dynamic input value to H3 tag
+//default 8 value of grid size in range
+gridNumber.textContent = gridRange.value;
+gridTitle.appendChild(gridNumber);
+
+//default 8*8 grids
+for (let row = 1; row <= 8; row++) {
+  for (let col = 1; col <= 8; col++) {
+    const column = document.createElement("div");
+    Object.assign(column.style, { border: "1px solid black", backgroundColor: "yellow", gridColumn: `${col} / span 1`, gridRow: `${row} / span 1` });
+    gridContainer.appendChild(column);
+  }
+}
+
+// Event listener: 1. Dynamic range value 2. Dynamic grid items
 gridRange.addEventListener("input", () => {
   gridNumber.textContent = gridRange.value;
   gridTitle.appendChild(gridNumber);
